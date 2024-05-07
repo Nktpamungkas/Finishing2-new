f721e9758d5484e19449cad8888afa5511e4e8b2
Deden.kurnia <nktpamungkas28@gmail.com>
2024-05-07 14:01:14 +0700

    penambahan refresh 180s di halaman status mesin

--- statusmesin/pages/home.php
+++ statusmesin/pages/home.php
@@ -7,6 +7,7 @@ include('../koneksi.php');
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="refresh" content="180">
     <title>Navbar di Tengah dengan Kolom dan Kotak Box</title>
     <style>
         /* Ini hanya contoh styling untuk tampilan */
