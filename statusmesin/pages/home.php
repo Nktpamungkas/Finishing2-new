83e076689075b4e9be160ec5ac4d37d127085d2d
Deden.kurnia <nktpamungkas28@gmail.com>
2024-05-07 15:06:44 +0700

    watu penambahan refresh 20s di halaman status mesin

--- statusmesin/pages/home.php
+++ statusmesin/pages/home.php
@@ -7,7 +7,7 @@ include('../koneksi.php');
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="refresh" content="180">
+    <meta http-equiv="refresh" content="20">
     <title>Navbar di Tengah dengan Kolom dan Kotak Box</title>
     <style>
         /* Ini hanya contoh styling untuk tampilan */
