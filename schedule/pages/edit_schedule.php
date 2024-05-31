c01a10e9277bae898ae6687ed74f4e27c27df03c
Deden.kurnia <nktpamungkas28@gmail.com>
2024-05-31 15:34:26 +0700

    menghilang where pada proses pilih dalam menu edit

--- schedule/pages/edit_schedule.php
+++ schedule/pages/edit_schedule.php
@@ -382,7 +382,7 @@ if (empty($_SESSION['usr'])) {
 						<select name="proses" id="proses" required  >
 							<option value="">Pilih</option>
 							<?php
-							$qry1 = mysqli_query($con, "SELECT proses,jns FROM tbl_proses WHERE ket='stenter' ORDER BY id ASC");
+							$qry1 = mysqli_query($con, "SELECT proses,jns FROM tbl_proses  ORDER BY id ASC");
 							while ($r = mysqli_fetch_array($qry1)) {
 							?>
 								<option value="<?php echo $r['proses'] . " (" . $r['jns'] . ")"; ?>" <?php if ($row_kkmasuk['proses'] == $r['proses'] . " (" . $r['jns'] . ")") {
